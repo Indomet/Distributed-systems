c67f76be8a5c042b6bc98956c0c7d87b5fa0fa28
Indomet <mehmetasim35@hotmail.com>
2024-01-07 14:01:44 +0100

    #4 remove unnecessary comments

--- locust-test/locustfile.py
+++ locust-test/locustfile.py
@@ -2,7 +2,7 @@ from locust import HttpUser, between, task
 
 
 class mapServiceTest(HttpUser):
-    wait_time = between(1, 5)
+    wait_time = between(1, 5) # random wait time(1 to 5 seconds) between each task
 
         
     @task
@@ -13,53 +13,12 @@ class mapServiceTest(HttpUser):
     def patient_get(self):
         self.client.get("/api/v1/patients/")
 
-    # @task
-    # def dentist_get(self):
-    #     self.client.get("/api/v1/dentists/")
-    
     @task
     def dentist_get(self):
         self.client.get("/api/v1/bookings/")
 
-    # ##map service
     # @task
-    # def map_post(self):
-    #     self.client.post("/api/v1/clinics",json={
-    #       "clinicName":"Lindholmen Dental Clinic",
-    #         "address": "Lindholmsplatsen 1",
-    #          "dentistId": "TEST ID",
-    #           "clinicEmail": "lindholmen@gmail.com"
-    # })
-    # @task
-    # def map_patch(self):
-    #     self.client.patch("/api/v1/clinics",json={
-    #     "address": "Östra Hamngatan 32"
-    # })
+    # def dentist_get(self):
+    #     self.client.get("/api/v1/dentists/")
     
-#     def on_start(self):
-#         self.client.post("/api/v1/clinics",{
-#     "clinicName":"Lindholmen Dental Clinic",
-#     "address": "Lindholmsplatsen 1",
-#     "dentistId": "TEST ID"
-# })
-    
-
-    # @task
-    # def userservice(self):
-    #     self.client.post("/api/v1/patients/",json={
-    #     "first_name": "azepei",
-    #     "last_name": "azhao",
-    #     "email": "a123@gmail.com",
-    #     "password": "a123",
-    #     "phone_number": 112234546464
-    # })
-        
-    # @task
-    # def get(self):
-    #     self.client.get("/api/v1/patients/")
-        
-
-    # @task
-    # def about(self):
-    #     self.client.get("/about/")
 
