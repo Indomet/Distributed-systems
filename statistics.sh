96227141028d06dac9024344d4e29bd01000c09e
Nasit Vurgun <nasit@student.chalmers.se>
2024-01-09 09:46:47 +0000

    Add real-time statistics script

---
+++ statistics.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while true; do
+    bookings=$(curl -s "http://192.168.96.193/api/v1/bookings/countAvailableBookings")
+    users=$(curl -s "http://192.168.96.193/userCount")
+  
+    echo "Available Bookings:"
+    echo "$bookings"
+
+    echo "Online users:"
+    echo "$users"
+
+    sleep 3
+done
+
+
+# chmod +x statistics/statistics.sh
+# ./statistics/statistics.sh
+
+
+# chmod +x statistics.sh
+# ./statistics.sh
